8cb166d5cfc2407eb01fd91343e4c03bb8529fa0
Isaac Jordan <isaacjordan95@gmail.com>
2020-01-03 08:44:11 +0000

    Set up valid S3 bucket name with script

--- set_up_new_project.sh
+++ set_up_new_project.sh
@@ -7,6 +7,10 @@ ORIGINAL_PROJECT_NAME="Django Prod QuickStart"
 PYTHON_MODULE_ORIGINAL_NAME=$(echo "ORIGINAL_PROJECT_NAME" | awk '{print tolower($0)}')
 PYTHON_MODULE_ORIGINAL_NAME=${ORIGINAL_PROJECT_NAME// /_}
 
+# Define PYTHON_MODULE_ORIGINAL_NAME as lowercase and replace all spaces with dashes
+S3_BUCKET_ORIGINAL_NAME=$(echo "ORIGINAL_PROJECT_NAME" | awk '{print tolower($0)}')
+S3_BUCKET_ORIGINAL_NAME=${S3_BUCKET_ORIGINAL_NAME// /-}
+
 # Check that DJANGO_PROD_QUICKSTART_NAME has been provided as an environment variable
 : "${DJANGO_PROD_QUICKSTART_NAME:?DJANGO_PROD_QUICKSTART_NAME must be set and non-empty. See README.}"
 
@@ -14,11 +18,18 @@ PYTHON_MODULE_ORIGINAL_NAME=${ORIGINAL_PROJECT_NAME// /_}
 PYTHON_MODULE_QUICKSTART_NAME=$(echo "$DJANGO_PROD_QUICKSTART_NAME" | awk '{print tolower($0)}')
 export PYTHON_MODULE_QUICKSTART_NAME=${PYTHON_MODULE_QUICKSTART_NAME// /_}
 
+# Define S3_BUCKET_QUICKSTART_NAME as lowercase and replace all spaces with dashes
+S3_BUCKET_QUICKSTART_NAME=$(echo "$DJANGO_PROD_QUICKSTART_NAME" | awk '{print tolower($0)}')
+S3_BUCKET_QUICKSTART_NAME=${S3_BUCKET_QUICKSTART_NAME// /-}
+
 echo "Using $PYTHON_MODULE_QUICKSTART_NAME as Python module name for your project."
 
 # Rename the Python module
 mv "$PYTHON_MODULE_ORIGINAL_NAME" "$PYTHON_MODULE_QUICKSTART_NAME"
 find . -name '*.{py,yml,json}' -exec sed -i '' -e "s/$PYTHON_MODULE_ORIGINAL_NAME/$PYTHON_MODULE_QUICKSTART_NAME/g" {} \;
+
+# Set up S3 bucket name
+find . -name 'zappa_settings.json' -exec sed -i '' -e "s/$S3_BUCKET_ORIGINAL_NAME/$S3_BUCKET_QUICKSTART_NAME/g" {} \;
 
 # Rename project name in templates
 find . -name '*.html' -exec sed -i '' -e "s/$ORIGINAL_PROJECT_NAME/$DJANGO_PROD_QUICKSTART_NAME/g" {} \;
